6f12674ccbaba97995ca8de4517007ff8a97d890
proushothkoushal <proushothkoushal@gmail.com>
2023-10-16 11:01:02 +0530

    .

---
+++ src/test/java/com/example/CustomerFeedback/CustomerFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomerFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
